7648ab84f5ffa2654e54b1c79efb427d1096773c
Nick Palladinos <nick.palladinos@gmail.com>
2016-06-04 17:41:06 +0300

    Add QueryTransformer basic structure and  basic syntax tree active patterns

---
+++ Looper.Core/QueryTransformer.fs
@@ -0,0 +1,24 @@
+﻿namespace Looper.Core
+
+    open Microsoft.CodeAnalysis
+    open Microsoft.CodeAnalysis.CSharp
+    open Microsoft.CodeAnalysis.CSharp.Syntax
+    open Looper.Core.SyntaxPatterns
+
+    module QueryTransformer = 
+
+        let intermediateMatch (node : SyntaxNode) : QueryExpr = 
+            match node with 
+            | InvocationExpression (MemberAccessExpression (IdentifierName "Select", expr), args) ->
+                failwith "oups" 
+            | _ -> failwith "oups" 
+
+        let consumerMatch (node : SyntaxNode) : QueryExpr = 
+            match node with 
+            | InvocationExpression (MemberAccessExpression (IdentifierName "Sum", expr), args) ->
+                intermediateMatch expr
+            | _ -> failwith "oups" 
+
+
+        let toQueryExpr (node : SyntaxNode) : QueryExpr =
+            consumerMatch node
