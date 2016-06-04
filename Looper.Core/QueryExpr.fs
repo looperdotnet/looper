e07d97f060334419c17635021ff478efb1398b35
Nick Palladinos <nick.palladinos@gmail.com>
2016-06-04 15:02:53 +0300

    Initial Looper.Core prj structure and QueryExpr ADT

---
+++ Looper.Core/QueryExpr.fs
@@ -0,0 +1,15 @@
+﻿namespace Looper.Core
+    open Microsoft.CodeAnalysis
+    open Microsoft.CodeAnalysis.CSharp
+    open Microsoft.CodeAnalysis.CSharp.Syntax
+
+    type QueryExpr =
+        // Intermediate 
+        | Select of SimpleLambdaExpressionSyntax * QueryExpr
+        | SelectMany of (ParameterSyntax * QueryExpr) * QueryExpr
+        | Filter of SimpleLambdaExpressionSyntax * QueryExpr
+        // Consumer
+        | ToArray of QueryExpr
+        | ToList of QueryExpr
+        | Sum of QueryExpr
+        | Count of QueryExpr
