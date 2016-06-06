8f8ce4057069f0f10daad894baaa23bf1fa643ea
Konstantinos Rontogiannis <krontogiannis@outlook.com>
2016-06-06 19:58:49 +0300

    Add F# PCL Looper

--- src/Looper/AssemblyInfo.fs
+++ src/Looper/AssemblyInfo.fs
@@ -1,5 +1,6 @@
-﻿namespace Looper.Core.AssemblyInfo
+﻿namespace Looper.AssemblyInfo
 
+open System.Resources
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
@@ -7,22 +8,15 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("Looper.Core")>]
+[<assembly: AssemblyTitle("Looper")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Looper.Core")>]
+[<assembly: AssemblyProduct("Looper")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("9f5d7067-63de-42f3-9dad-30520202abc9")>]
+[<assembly: NeutralResourcesLanguage("en")>]
 
 // Version information for an assembly consists of the following four values:
 // 
