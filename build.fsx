ffc0dd35cb09424068d0942b6e134e32d86eaae8
Konstantinos Rontogiannis <krontogiannis@outlook.com>
2016-06-07 18:52:32 +0300

    Add FAKE

---
+++ build.fsx
@@ -0,0 +1,27 @@
+﻿#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
+open Fake.UserInputHelper
+open System
+open System.IO
+
+
+let solutionFile  = "Looper.sln"
+
+Target "Clean" (fun _ -> CleanDirs ["bin"])
+
+Target "Build" (fun _ ->
+    !! solutionFile
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore
+)
+
+Target "Default" DoNothing
+
+"Clean"
+  ==> "Build"
+  ==> "Default"
+
+RunTargetOrDefault "Default"
