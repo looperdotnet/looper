7522b5d9036ec1824bb6037868d44409af1fe145
Konstantinos Rontogiannis <krontogiannis@outlook.com>
2016-06-08 20:57:48 +0300

    Add xunit to fake

--- build.fsx
+++ build.fsx
@@ -1,17 +1,14 @@
 ﻿#r @"packages/FAKE/tools/FakeLib.dll"
 open Fake
-open Fake.Git
-open Fake.AssemblyInfoFile
-open Fake.ReleaseNotesHelper
-open Fake.UserInputHelper
-open System
-open System.IO
+open Fake.Testing
 
-
-let buildDir = "bin"
+let binDir = "bin"
+let debugDir = binDir @@ "Debug"
+let releaseDir = binDir @@ "Release"
+let testArtifacts = binDir @@ "test"
 let solutionFile = "Looper.sln"
 
-Target "Clean" (fun _ -> CleanDirs [buildDir])
+Target "Clean" (fun _ -> CleanDirs [binDir])
 
 Target "Core" (fun _ ->
     !! "src/**/LooperAnalyzer.csproj"
@@ -19,19 +16,33 @@ Target "Core" (fun _ ->
         |> Log "AppBuild-Output: "
 )
 
-Target "Build" (fun _ ->
+Target "Tests" (fun _ ->
+    !! "tests/**/*.csproj"
+        |> MSBuildDebug "" "Build"
+        |> Log "AppBuild-Output: "    
+)
+
+Target "All" (fun _ ->
     !! solutionFile
     |> MSBuildRelease "" "Rebuild"
     |> ignore
 )
 
+Target "Test" (fun _ ->
+    //tracefn "%A" (debugDir @@ "*.Test.exe")
+    !! (debugDir @@ "*.Test.exe")
+    |> xUnit2 (fun p -> { p with HtmlOutputPath = Some(testArtifacts @@ "xunit.html") })
+)
+
 Target "Default" DoNothing
 
-"Clean"
-    ==> "Core"
-    ==> "Default"
+Target "List" PrintTargets
 
-"Clean"
-    ==> "Build"
+"Clean" ==> "Core" ==> "Default"
+
+"Clean" ==> "All"
+
+"Clean" ==> "Tests" ==> "Test"
 
 RunTargetOrDefault "Default"
+
