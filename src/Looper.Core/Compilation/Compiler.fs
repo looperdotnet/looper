a570f28081ba186fa39ac30bbc26bce9aafea1fa
Nick Palladinos <nick.palladinos@gmail.com>
2016-06-12 16:31:45 +0300

    Compiler For loop transformation (WIP)

--- src/Looper.Core/Compilation/Compiler.fs
+++ src/Looper.Core/Compilation/Compiler.fs
@@ -4,6 +4,7 @@
     open Microsoft.CodeAnalysis.CSharp
     open Microsoft.CodeAnalysis.CSharp.Syntax
     open System
+    open Looper.Core.SymbolUtils
 
 
     let private throwNotImplemented =
@@ -12,10 +13,26 @@
                 SyntaxFactory.IdentifierName("System.NotImplementedException"))
                 .WithArgumentList(SyntaxFactory.ArgumentList())) :> SyntaxNode
 
+    let syntaxList<'Node when 'Node :> SyntaxNode> (elements : seq<'Node>) = 
+        SyntaxFactory.SeparatedList(elements)
+    let varStmt = 
+        SyntaxFactory.VariableDeclaration(
+            SyntaxFactory.IdentifierName("var")).
+            WithVariables(
+            SyntaxFactory.SingletonSeparatedList(
+                SyntaxFactory.VariableDeclarator(
+                    SyntaxFactory.Identifier("__i__")). // TODO: replace with fresh names
+                    WithInitializer(
+                    SyntaxFactory.EqualsValueClause(SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(0)))))).NormalizeWhitespace()
+    let forStmt = 
+        let lessThenExpr = SyntaxFactory.BinaryExpression(SyntaxKind.LessThanExpression, SyntaxFactory.IdentifierName("__i__"), SyntaxFactory.IdentifierName("__i__"))
+        SyntaxFactory.ForStatement(varStmt, syntaxList [||], lessThenExpr, syntaxList [||], SyntaxFactory.EmptyStatement())
+
     let rec compileQuery (query : QueryExpr) (model : SemanticModel) (k : SyntaxNode -> SyntaxNode) : SyntaxNode =
+        
         match query with
-        | SourceIdentifierName (typeSymbol, identifier) ->
-            throwNotImplemented
+        | SourceIdentifierName (ArrayType arrayTypeSymbol, identifier) ->    
+            varStmt :> _
         | Select (Lambda (param, body), query) ->
              compileQuery query model k
         | Sum query -> 
@@ -25,5 +42,5 @@
     let compile (query : StmtQueryExpr) (model : SemanticModel) : SyntaxNode =
         match query with
         | Assign (typeSyntax, typeSymbol, identifier, queryExpr) -> 
-            throwNotImplemented
+            compileQuery queryExpr model id 
         | _ -> throwNotImplemented
