d67b8a809146b0e80bd07b130d18c963a8453ece
Nick Palladinos <nick.palladinos@gmail.com>
2016-06-25 21:17:36 +0300

    Add SelectMany pattern to QueryTransformer

--- src/Looper.Core/Analysis/QueryTransformer.fs
+++ src/Looper.Core/Analysis/QueryTransformer.fs
@@ -5,7 +5,7 @@
     open Microsoft.CodeAnalysis.CSharp.Syntax
     open Looper.Core.SyntaxPatterns
 
-    let private (|LinqInvocation|_|) (checker : SymbolChecker) = 
+    let rec private (|LinqInvocation|_|) (checker : SymbolChecker) = 
         function 
         | InvocationExpression(MemberAccessExpression(IdentifierName name, expr) as memberExpr, args) -> 
             let symbol = checker.SemanticModel.GetSymbolInfo(memberExpr).Symbol
@@ -14,7 +14,7 @@
             | _ -> None
         | _ -> None
 
-    let private producerMatch (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option =
+    and private producerMatch (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option =
         match node with 
         | IdentifierName _ -> 
             match checker.SemanticModel.GetSymbolInfo(node).Symbol with
@@ -23,15 +23,17 @@
         | :? ExpressionSyntax as expr -> Some(SourceExpression expr)
         | _ -> None
 
-    let rec private intermediateMatch (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option = 
+    and private intermediateMatch (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option = 
         match node with 
         | LinqInvocation checker (expr, "Select", [SimpleLambdaExpression (param, body)]) ->
             intermediateMatch expr checker |> Option.map (fun expr -> Select (Lambda (param, body), expr))
         | LinqInvocation checker (expr, "Where", [SimpleLambdaExpression (param, body)]) ->
             intermediateMatch expr checker |> Option.map (fun expr -> Where (Lambda (param, body), expr))
+        | LinqInvocation checker (expr, "SelectMany", [SimpleLambdaExpression (param, NestedQueryExpr checker body)]) ->
+            intermediateMatch expr checker |> Option.map (fun expr -> SelectMany ((param, body), expr))
         | _ -> producerMatch node checker
 
-    let private consumerMatch (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option = 
+    and private consumerMatch (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option = 
         match node with 
         | LinqInvocation checker (expr, "Sum", []) ->
             intermediateMatch expr checker |> Option.map Sum 
@@ -41,28 +43,31 @@
             intermediateMatch expr checker |> Option.map Any
         | _ -> None
 
-    let toQueryExpr (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option =
+    and toQueryExpr (node : SyntaxNode) (checker : SymbolChecker) : QueryExpr option =
         consumerMatch node checker
 
-    let (|QueryExpr|_|) (checker : SymbolChecker) (node : SyntaxNode) =
+    and (|QueryExpr|_|) (checker : SymbolChecker) (node : SyntaxNode) =
         toQueryExpr node checker
 
-    let toStmtQueryExpr (node : SyntaxNode) (checker : SymbolChecker) : StmtQueryExpr option =
+    and (|NestedQueryExpr|_|) (checker : SymbolChecker) (node : SyntaxNode) =
+        intermediateMatch node checker
+
+    and toStmtQueryExpr (node : SyntaxNode) (checker : SymbolChecker) : StmtQueryExpr option =
         match node with
         | LocalDeclarationStatement (_, VariableDeclaration (typeSyntax, [VariableDeclarator (identifier, _, EqualsValueClause (QueryExpr checker expr))])) -> 
             let typeSymbol = checker.SemanticModel.GetSymbolInfo(typeSyntax).Symbol :?> INamedTypeSymbol
             Some (Assign (typeSyntax, typeSymbol, identifier, expr))
         | _ -> None
 
-    let (|StmtQueryExpr|_|) (checker : SymbolChecker) (node : SyntaxNode) =
+    and (|StmtQueryExpr|_|) (checker : SymbolChecker) (node : SyntaxNode) =
         toStmtQueryExpr node checker
 
-    let (|NoConsumerQuery|_|) (checker : SymbolChecker) (node : SyntaxNode) =
+    and (|NoConsumerQuery|_|) (checker : SymbolChecker) (node : SyntaxNode) =
         match intermediateMatch node checker with
         | Some _ as m when producerMatch node checker = None -> m
         | _ -> None
 
-    let (|StmtNoConsumerQuery|_|) (checker : SymbolChecker) (node : SyntaxNode)  =
+    and (|StmtNoConsumerQuery|_|) (checker : SymbolChecker) (node : SyntaxNode)  =
         match node with
         | LocalDeclarationStatement (_, VariableDeclaration (typeSyntax, [VariableDeclarator (identifier, _, EqualsValueClause (NoConsumerQuery checker expr))])) -> 
             let typeSymbol = checker.SemanticModel.GetSymbolInfo(typeSyntax).Symbol :?> ITypeSymbol
--- tests/LooperAnalyzer.Test/CodegenTests.cs
+++ tests/LooperAnalyzer.Test/CodegenTests.cs
@@ -59,5 +59,11 @@ namespace LooperAnalyzer.Test
             await VerifyCodeGen<int>(template,
                 inits: new[] { "var xs = new [] { 1, 2, 3 };" },
                 linqExpr: "xs.Where(x => x % 2 == 0).Sum()");
+
+        [Theory(DisplayName = "SelectMany"), MemberData(nameof(Templates))]
+        public async Task SelectMany(CodeGenTemplate template) =>
+            await VerifyCodeGen<int>(template,
+                inits: new[] { "var xs = new [] { 1, 2, 3 };" },
+                linqExpr: "xs.SelectMany(n => Enumerable.Range(1, n).Select(x => x + 1)).Sum()");
     }
 }
