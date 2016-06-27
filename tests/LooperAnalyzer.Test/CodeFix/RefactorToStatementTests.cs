2160b92fc02853772f1cdfbe7fc6149a136fb732
Konstantinos Rontogiannis <krontogiannis@outlook.com>
2016-06-27 14:08:50 +0300

    Add refactoring tests

--- tests/LooperAnalyzer.Test/CodeFix/RefactorToStatementTests.cs
+++ tests/LooperAnalyzer.Test/CodeFix/RefactorToStatementTests.cs
@@ -31,7 +31,66 @@ namespace LooperAnalyzer.Test
                 Locations = new[] { new DiagnosticResultLocation("Test0.cs", row, col) }
             };
 
-        //Diagnostic and CodeFix both triggered and checked for
+        [Fact(DisplayName = "QueryExprStmt declaration should not produce refactoring diagnostics")]
+        public void DeclarationNoDiagnostics()
+        {
+            var test = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        var xs = Enumerable.Range(1,10).Count();
+    }
+}
+";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [Fact(DisplayName = "QueryExprStmt assignment should not produce refactoring diagnostics")]
+        public void AssignNoDiagnostics()
+        {
+            var test = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        int xs;
+        xs = Enumerable.Range(1,10).Count();
+    }
+}
+";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [Fact(DisplayName = "Refactoring fresh variable name should no collide with scope")]
+        public void FreshVarCollision()
+        {
+            var test = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        int count;
+        var xs = 1 + Enumerable.Range(1,10).Count();
+    }
+}
+";
+            VerifyCSharpDiagnostic(test, Expected(7, 22));
+
+            var fixtest = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        int count;
+        var count0 = Enumerable.Range(1,10).Count();
+        var xs = 1 + count;
+    }
+}
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
         [Fact(DisplayName = "Refactor expression from simple if statement")]
         public void IfStatement()
         {
@@ -62,8 +121,95 @@ class TestClass
     }
 }
 ";
-
             VerifyCSharpFix(test, fixtest);
         }
+
+        [Fact(DisplayName = "Refactor expression from local declaration")]
+        public void LocalDeclaration()
+        {
+            var test = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        var xs = 1 + Enumerable.Range(1,10).Count();
+    }
+}
+";
+            VerifyCSharpDiagnostic(test, Expected(6, 22));
+
+            var fixtest = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        var count = Enumerable.Range(1,10).Count();
+        var xs = 1 + count;
+    }
+}
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [Fact(DisplayName = "Refactor expression from assignment")]
+        public void Assignment()
+        {
+            var test = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        int xs;
+        xs = 1 + Enumerable.Range(1,10).Count();
+    }
+}
+";
+            VerifyCSharpDiagnostic(test, Expected(7, 9));
+
+            var fixtest = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        int xs;
+        var count = Enumerable.Range(1,10).Count();
+        xs = 1 + count;
+    }
+}
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        [Fact(DisplayName = "Refactor expression from do-while")]
+        public void DoStatement()
+        {
+            var test = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        do {
+            var x = 42;
+        } while(xs = 1 + Enumerable.Range(1,x).Count());
+    }
+}
+";
+            VerifyCSharpDiagnostic(test, Expected(7, 13));
+
+            var fixtest = @"
+using System.Linq;
+class TestClass
+{
+    void Test() {
+        do {
+            var x = 42;
+            var count = Enumerable.Range(1, x).Count();
+        } while(xs = 1 + count);
+    }
+}
+";
+            VerifyCSharpFix(test, fixtest);
+        }
+
     }
 }
